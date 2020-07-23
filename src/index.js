f2ec82fe1268eae3de4708c74f4b1d2c4879acd7
Lewis Marshall <marshy2201@yahoo.co.uk>
2020-07-23 16:26:02 +0100

    window scroll to top when navigating

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>,
