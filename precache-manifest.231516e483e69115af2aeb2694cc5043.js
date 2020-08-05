5ddaf742eac4a354aaa7326814bf554c29a189d8
Lewis Marshall <marshy2201@yahoo.co.uk>
2020-08-05 12:47:45 +0100

    Updates

---
+++ precache-manifest.231516e483e69115af2aeb2694cc5043.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cf7e97e1a68cfc83efc45105524b1a",
+    "url": "/react-treehouse/index.html"
+  },
+  {
+    "revision": "cb154ffbdd0ba4c7ca7c",
+    "url": "/react-treehouse/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c6e02fcdb9be0c155ad",
+    "url": "/react-treehouse/static/css/main.ea75b6c0.chunk.css"
+  },
+  {
+    "revision": "cb154ffbdd0ba4c7ca7c",
+    "url": "/react-treehouse/static/js/2.195200e1.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/react-treehouse/static/js/2.195200e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6e02fcdb9be0c155ad",
+    "url": "/react-treehouse/static/js/main.108b1d31.chunk.js"
+  },
+  {
+    "revision": "e40e5c0b25a97504716f",
+    "url": "/react-treehouse/static/js/runtime-main.dae7aa52.js"
+  },
+  {
+    "revision": "b78431012a1677e75951ecbd4f6b7046",
+    "url": "/react-treehouse/static/media/logo.b7843101.svg"
+  }
+]);
